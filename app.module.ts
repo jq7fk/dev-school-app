9fd9ecc3f735bc3fdb3dfec9d22e5084ea84cbf1
ep8pm <ep8pm@virginia.edu>
2019-06-11 14:06:11 -0500

    created dash

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './card.component';
+import { NavbarComponent } from './navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
