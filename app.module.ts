23ee3199e35b1bc60ed01aa24d4a8ac5fb442d0c
ep8pm <ep8pm@virginia.edu>
2019-06-11 16:22:03 -0500

    Added more info page
    all of the components start with info-

--- app.module.ts
+++ app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './card.component';
 import { NavbarComponent } from './navbar.component';
 import { CreateComponent } from './create.component';
+import { InfocardComponent } from './infocard.component';
+import { InfocommentComponent } from './infocomment.component';
+import { InfonavbarComponent } from './infonavbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     NavbarComponent,
-    CreateComponent
+    CreateComponent,
+    InfocardComponent,
+    InfocommentComponent,
+    InfonavbarComponent,
+
   ],
   imports: [
     BrowserModule,
---
+++ infonavbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infonavbar',
+  templateUrl: './infonavbar.component.html',
+})
+export class InfonavbarComponent {
+
+}
