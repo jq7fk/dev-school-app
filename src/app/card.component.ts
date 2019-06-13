948ad43130aada1161b2d4243b8e9b6cc863711a
EP8PM <ep8pm@virginia.edu>
2019-06-13 15:41:45 -0500

    edit button

--- src/app/card.component.ts
+++ src/app/card.component.ts
@@ -3,6 +3,8 @@ import { Event} from './event';
 import { User } from './user'
 import { events } from './eventsdata';
 import { Router } from '@angular/router';
+import { users } from './usersdata';
+
 
 @Component({
   selector: 'app-card',
@@ -12,20 +14,26 @@ export class CardComponent implements OnInit {
   @Input() event: Event;
   @Input() joined: boolean;
 
+  creator = false;
   eventsList = events;
-  currentUser = new User(5, "Current", "User", "Dallas");
+  currentUser = users[1];
 
   constructor(private route: Router) {
   }
 
   ngOnInit() {
+    if (this.currentUser === this.event.creator) {
+      this.creator = true;
+    }
   }
 
   join() {
     this.joined = true;
     this.eventsList.find(item => item.id === this.event.id).attendees.push(this.currentUser);
   }
-
+  editClick() {
+    this.route.navigate(['/editEvent', this.event.id]);
+  }
   commentsClick() {
     this.route.navigate(['/details', this.event.id]);
   }
