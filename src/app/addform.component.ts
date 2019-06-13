e5531028ae57531045a8b15c2f3bf749e9460d39
ep8pm <ep8pm@virginia.edu>
2019-06-13 10:23:23 -0500

    addform edits

--- src/app/addform.component.ts
+++ src/app/addform.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { Time } from '@angular/common';
 import { Event } from './event';
+import { NgModel } from '@angular/forms';
+import { User } from './user';
 
 @Component({
   selector: 'app-addform',
   templateUrl: './addform.component.html',
 })
 export class AddformComponent {
-    // get data from the form
-        // var
-        // bind with html elements
-    // create a new event object from data
+  //hardcoded users
+  user = new User(1, 'Edmund', 'Pan', 'Houston');
+  user1 = new User(1, 'Jennifer', 'Qian', 'D.C.');
+  user2 = new User(1, 'Matt', 'Damon', 'Houston');
+  user3 = new User(1, 'Chris', 'Pratt', 'Houston');
+  going = [this.user1, this.user2, this.user3];
+  model = new Event(1, '', '', '', '', this.user , this.going, '', '');
+  submitted = false;
+  newEvent() {
+    this.model = new Event(1, '', '', '', '', this.user , this.going, '', '');
+    this.submitted = true; }
 
-    ngOnInit() {
-    }
-
-    onSubmit() {
-
-    }
 }
